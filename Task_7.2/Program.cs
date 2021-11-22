4baf6c669158ac0788497e9ad8657d7d30e0c542
Максим Белов <makbe@yandex.ru>
2021-11-23 00:42:06 +0300

    Добавьте файлы проекта.

---
+++ Task_7.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            CalcCube(a, out double V, out double S);
+            Console.WriteLine();
+            Console.WriteLine("Объем куба: {0}", V);
+            Console.WriteLine("Площадь поверхности куба: {0}", S);
+            Console.ReadKey();
+        }
+        static void CalcCube (double a, out double V, out double S)
+        {
+            V = Math.Round(Math.Pow(a, 3), 2);
+            S = 6 * a * a;
+        }
+
+    }
+}
